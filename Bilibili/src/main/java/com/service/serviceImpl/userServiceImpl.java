df928d0dee0d7a3a84ea483506239b159e6b0189
130B848 <592ldj227@gmail.com>
2016-06-30 09:35:54 +0800

    log_in action added.

--- Bilibili/src/main/java/com/service/serviceImpl/userServiceImpl.java
+++ Bilibili/src/main/java/com/service/serviceImpl/userServiceImpl.java
@@ -1,19 +1,40 @@
 package com.service.serviceImpl;
 
-import com.dao.daoImpl.userDAOImpl;
+import com.dao.userDAO;
+import com.pojo.User;
 import com.service.userService;
 
 /**
  * Created by Admin on 2016/6/27.
  */
 public class userServiceImpl implements userService {
-    private userDAOImpl userDAO;
+    private userDAO userDAO;
 
-    public void setUserDAO(userDAOImpl userDAO) {
+    public void setUserDAO(userDAO userDAO) {
         this.userDAO = userDAO;
     }
 
-    public userDAOImpl getUserDAO() {
+    public userDAO getUserDAO() {
         return userDAO;
+    }
+
+    @Override
+    public void createUser(User user) {
+        userDAO.createUser(user);
+    }
+
+    @Override
+    public void deleteUser(int userId) {
+        userDAO.deleteUser(userId);
+    }
+
+    @Override
+    public void updateUser(User user) {
+        userDAO.updateUser(user);
+    }
+
+    @Override
+    public User findUserById(int userId) {
+        return userDAO.findUserById(userId);
     }
 }
--- Bilibili/src/main/java/com/service/userService.java
+++ Bilibili/src/main/java/com/service/userService.java
@@ -1,7 +1,13 @@
 package com.service;
 
+import com.pojo.User;
+
 /**
  * Created by Admin on 2016/6/27.
  */
 public interface userService {
+    public void createUser(User user);
+    public void deleteUser(int userId);
+    public void updateUser(User user);
+    public User findUserById(int userId);
 }
