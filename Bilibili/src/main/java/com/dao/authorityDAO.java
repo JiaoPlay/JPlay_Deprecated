dbc30016d6253a8b7ce28aad1c6f2d0a0935ad36
thebestsunny <frank_xiang@sjtu.edu.cn>
2016-06-30 11:13:27 +0800

    Modify authorityDAO.java.

--- Bilibili/src/main/java/com/dao/authorityDAO.java
+++ Bilibili/src/main/java/com/dao/authorityDAO.java
@@ -5,6 +5,11 @@ import com.pojo.Authority;
 /**
  * Created by frank_xiang on 2016/6/28.
  */
+
+
+/**
+ * authorityDAO接口 提供了对authority进行增删改查功能的接口，将在daoImpl包中进行具体的实现。
+ */
 public interface authorityDAO {
     public void createAuthority(Authority authority);
     public void deleteAuthority(int authId);
