848b5ae5d3ccdbc7fb5751001a469b6f896a7498
130B848 <592ldj227@gmail.com>
2016-07-03 20:11:37 +0800

    Mongodb Template is available now.

--- Bilibili/src/main/java/com/util/uploadTest.java
+++ Bilibili/src/main/java/com/util/uploadTest.java
@@ -1,12 +1,13 @@
 package com.util;
 
 import java.io.File;
+import java.net.UnknownHostException;
 
 /**
  * Created by Admin on 2016/6/29.
  */
 public class uploadTest {
-    public static void main(String args[]){
+    public static void main(String args[]) throws UnknownHostException {
 //        uploadHandler uploadHandler = new uploadHandler();
 //        uploadHandler.setUploadFile(new File("D:\\BaiduYunDownload\\Railgun_II_2160p_120fps.mkv"));
 //        uploadHandler.setFilename("copy.mkv");
@@ -18,5 +19,6 @@ public class uploadTest {
 //        } finally {
 //            System.out.println("End.");
 //        }
+//        MongoClient mongoClient = new MongoClient("localhost", 27017);
     }
 }
